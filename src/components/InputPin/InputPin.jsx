81fb1c6d6e5dec0be042b1cc32acbaa30385ab5c
XΛVI <hello@soyjavi.com>
2024-01-08 21:50:42 +0700

    🆕 component <InputPin>

---
+++ src/components/InputPin/InputPin.jsx
@@ -0,0 +1,66 @@
+import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { style } from './InputPin.style';
+import { Input, View } from '../../__primitives__';
+
+const InputPin = ({ length = 6, onChange = () => {}, ...others }) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const refs = Array.from({ length }).map(() => useRef(null));
+
+  const [values, setValues] = useState(Array(length).fill(''));
+
+  useEffect(() => {
+    onChange(!values.includes('') ? values.join('') : '');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [values]);
+
+  const handleChange = (value = '', index) => {
+    const next = [...values];
+    next[index] = value;
+    setValues(next);
+
+    if (value.length === 1 && index < refs.length - 1) {
+      refs[index + 1].current.focus();
+    }
+  };
+
+  const handleKeyPress = ({ nativeEvent: { key } = {} } = {}, index) => {
+    if (key === 'Backspace' && index > 0) {
+      const next = [...values];
+      next[index] = '';
+      setValues(next);
+
+      refs[index - 1].current.focus();
+    }
+  };
+
+  return (
+    <View {...others} row spaceBetween wide>
+      {refs.map((inputRef, index) => (
+        <Input
+          align="center"
+          key={index}
+          keyboard="numeric"
+          maxLength={1}
+          ref={inputRef}
+          secureTextEntry
+          value={values[index]}
+          valid={values[index] !== ''}
+          onChange={(value) => handleChange(value, index)}
+          onKeyPress={(event) => handleKeyPress(event, index)}
+          style={style.input}
+        />
+      ))}
+    </View>
+  );
+};
+
+InputPin.displayName = 'InputPin';
+
+InputPin.propTypes = {
+  length: PropTypes.number,
+  onChange: PropTypes.func,
+};
+
+export { InputPin };
---
+++ src/components/InputPin/InputPin.style.js
@@ -0,0 +1,8 @@
+import StyleSheet from 'react-native-extended-stylesheet';
+
+export const style = StyleSheet.create({
+  input: {
+    height: '$spaceL * 2',
+    maxWidth: '$spaceL * 2',
+  },
+});
