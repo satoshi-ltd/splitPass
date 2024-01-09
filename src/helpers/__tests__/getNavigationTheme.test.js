be7849083d2a3e1543786b831fe4eb6b0c5fbd24
XΛVI <hello@soyjavi.com>
2024-01-09 21:04:40 +0700

    Naive testing environment with jest-expo

---
+++ src/helpers/__tests__/getNavigationTheme.test.js
@@ -0,0 +1,7 @@
+import { getNavigationTheme } from '../getNavigationTheme';
+
+describe('helpers/getNavigationTheme', () => {
+  it('has 1 child', () => {
+    expect(getNavigationTheme).toBeDefined();
+  });
+});
