be07ceccbcc908d66cd52617fa73ee695a1890cb
XΛVI <hello@soyjavi.com>
2024-01-11 08:46:39 +0700

    Improved Cypher

---
+++ src/helpers/__tests__/cypher.test.js
@@ -0,0 +1,51 @@
+import { Cypher } from '../cypher';
+
+describe('helpers/cypher', () => {
+  test('alive', () => {
+    expect(Cypher).toBeDefined();
+  });
+
+  describe('encrypt', () => {
+    test('should encrypt the value with the provided pin', () => {
+      expect(Cypher.encrypt('123456789', '123456')).toBe('246802802');
+    });
+
+    test('should return undefined when value is not a number', () => {
+      expect(Cypher.encrypt(123456789, 123456)).toBeUndefined();
+      expect(Cypher.encrypt('string', 123456)).toBeUndefined();
+      expect(Cypher.encrypt([], 123456)).toBeUndefined();
+      expect(Cypher.encrypt({}, 123456)).toBeUndefined();
+      expect(Cypher.encrypt(true, 123456)).toBeUndefined();
+    });
+
+    test('should return undefined when pin is not a valid format', () => {
+      expect(Cypher.encrypt(123456789, 123456)).toBeUndefined();
+      expect(Cypher.encrypt(123456789, 'string')).toBeUndefined();
+      expect(Cypher.encrypt(123456789, [])).toBeUndefined();
+      expect(Cypher.encrypt(123456789, {})).toBeUndefined();
+      expect(Cypher.encrypt(123456789, true)).toBeUndefined();
+    });
+  });
+
+  describe('decrypt', () => {
+    test('should decrypt the value with the provided pin', () => {
+      expect(Cypher.decrypt('246802802', '123456')).toBe('123456789');
+    });
+
+    test('should return undefined when value is not a number', () => {
+      expect(Cypher.decrypt(246802802, 123456)).toBeUndefined();
+      expect(Cypher.decrypt('string', 123456)).toBeUndefined();
+      expect(Cypher.decrypt([], 123456)).toBeUndefined();
+      expect(Cypher.decrypt({}, 123456)).toBeUndefined();
+      expect(Cypher.decrypt(true, 123456)).toBeUndefined();
+    });
+
+    test('should return undefined when pin is not a valid format', () => {
+      expect(Cypher.decrypt(123456789, 123456)).toBeUndefined();
+      expect(Cypher.decrypt(123456789, 'string')).toBeUndefined();
+      expect(Cypher.decrypt(123456789, [])).toBeUndefined();
+      expect(Cypher.decrypt(123456789, {})).toBeUndefined();
+      expect(Cypher.decrypt(123456789, true)).toBeUndefined();
+    });
+  });
+});
---
+++ src/helpers/cypher.js
@@ -0,0 +1,24 @@
+const convertToDigits = (value = '') => value.split('').map(Number);
+
+const invalidInputs = (value, pin) =>
+  typeof value !== 'string' || !/^\d+$/.test(value) || typeof pin !== 'string' || !/^\d{6}$/.test(pin);
+
+export const Cypher = {
+  encrypt: (value = '', pin = '') => {
+    if (invalidInputs(value, pin)) return;
+    const pinDigits = convertToDigits(pin);
+
+    return convertToDigits(value)
+      .map((digit, index) => (digit + pinDigits[index % 6]) % 10)
+      .join('');
+  },
+
+  decrypt: (value = '', pin = '') => {
+    if (invalidInputs(value, pin)) return;
+    const pinDigits = convertToDigits(pin);
+
+    return convertToDigits(value)
+      .map((digit, index) => (digit - pinDigits[index % 6] + 10) % 10)
+      .join('');
+  },
+};
