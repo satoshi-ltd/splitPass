6c3fbe0b1c9bf82f986260ad263c038d96b53aea
Mikel Garcia <mikelgarciaurbina@gmail.com>
2024-01-12 15:55:36 +0700

    Add popup with QR code scanner to the Icon

--- src/extension/App.jsx
+++ src/extension/App.jsx
@@ -1,10 +1,12 @@
+import { QrScanner } from '@yudiel/react-qr-scanner';
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import StyleSheet from 'react-native-extended-stylesheet';
-// import { QrScanner } from '@yudiel/react-qr-scanner';
 
-import { Text } from '../__primitives__/Text';
-import { getPositionIconStyle } from './helpers/getPositionIconStyle';
+import { style } from './App.style';
 import IconPNG from './assets/icon.png';
+import { getPositionIconStyle } from './helpers/getPositionIconStyle';
+import { Popup } from '../components/Popup';
 import { defaultTheme } from '../themes/default.theme';
 
 StyleSheet.build(defaultTheme);
@@ -13,43 +15,32 @@ function App({ passwordField }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const iconStyle = getPositionIconStyle(passwordField);
-  // console.log('iconStyle', iconStyle);
 
-  // const onDecode = (result) => {
-  //   console.log(result);
-  //   setIsOpen(false);
-  //   var passwordFieldElement = document.getElementById(passwordFieldId);
-  //   passwordFieldElement.value = result;
-  // };
-  // const onError = (error) => {
-  //   console.log(error?.message);
-  // };
-
-  // const popupStyle = {
-  //   display: 'block',
-  //   position: 'absolute',
-  //   left: '50px',
-  //   top: '50px',
-  //   width: '300px',
-  //   height: '300px',
-  //   backgroundColor: 'white',
-  //   border: '1px solid black',
-  //   padding: '20px',
-  //   zIndex: 1000,
-  //   boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  // };
+  const onDecode = (result) => {
+    console.log(result);
+    setIsOpen(false);
+    passwordField.value = result;
+  };
+  const onError = (error) => {
+    console.log(error?.message);
+  };
 
   return (
     <>
-      <img alt="icon" onClick={() => setIsOpen(true)} src={IconPNG} style={iconStyle} />
-      <Text>Holaaaaa</Text>
-      {/* {isOpen && (
-        <div style={popupStyle}>
-          <QrScanner onDecode={onDecode} onError={onError} scanDelay={2000} stopDecoding={!isOpen} />
-        </div>
-      )} */}
+      <Popup
+        content={<QrScanner onDecode={onDecode} onError={onError} scanDelay={2000} stopDecoding={!isOpen} />}
+        isOpen={isOpen}
+        popupStyle={style.container}
+        style={iconStyle}
+      >
+        <img alt="icon" onClick={() => setIsOpen(!isOpen)} src={IconPNG} />
+      </Popup>
     </>
   );
 }
+
+App.propTypes = {
+  passwordField: PropTypes.node,
+};
 
 export default App;
---
+++ src/extension/App.style.js
@@ -0,0 +1,8 @@
+import StyleSheet from 'react-native-extended-stylesheet';
+
+export const style = StyleSheet.create({
+  container: {
+    width: 250,
+    transform: 'translate(-50%, 0px)',
+  },
+});
