32d662ace17cccfc6fb8d052274337f8fde8d1f3
XΛVI <hello@soyjavi.com>
2024-01-13 21:54:57 +0700

    <QR> improvements

--- src/components/QR/QR.jsx
+++ src/components/QR/QR.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 import StyleSheet from 'react-native-extended-stylesheet';
 import QRCodeStyled from 'react-native-qrcode-styled';
 
+import { style } from './Qr.style';
 import { View } from '../../__primitives__';
 
 const QR = ({ rounded = true, size = 128, value = '', ...others }) => {
-  const pieceSize = size / 32;
-  const borderRadius = rounded ? pieceSize / 2 : undefined;
+  const { length = 1 } = value;
+  const pieceSize = parseInt(size / (length >= 96 ? 42 : length >= 48 ? 34 : 28));
+  const borderRadius = rounded ? parseInt(pieceSize / 2) : undefined;
 
   return (
-    <View {...others}>
+    <View {...others} style={[style.container, { height: size, width: size }, others.className]}>
       <QRCodeStyled
         color={StyleSheet.value('$qrColor')}
         data={value}
@@ -19,10 +21,7 @@ const QR = ({ rounded = true, size = 128, value = '', ...others }) => {
         pieceBorderRadius={borderRadius}
         pieceCornerType={rounded ? 'rounded' : undefined}
         pieceSize={pieceSize}
-        style={{
-          backgroundColor: StyleSheet.value('$qrBackgroundColor'),
-          borderRadius,
-        }}
+        style={{ backgroundColor: StyleSheet.value('$qrBackgroundColor') }}
       />
     </View>
   );
---
+++ src/components/QR/Qr.style.jsx
@@ -0,0 +1,10 @@
+import StyleSheet from 'react-native-extended-stylesheet';
+
+export const style = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '$qrBackgroundColor',
+    borderRadius: '$borderRadius',
+    justifyContent: 'center',
+  },
+});
