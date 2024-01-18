cadbfce048a87a037663f44462affcefedf37193
Mikel Garcia <mikelgarciaurbina@gmail.com>
2024-01-18 14:42:06 +0700

    Add react native svg mocks to work with parcel

---
+++ src/mocks/mockCodeGenNativeComponent.js
@@ -0,0 +1,7 @@
+// mockCodeGenNativeComponent.js
+const codegenNativeComponent = () => {
+  // Mock implementation or return a dummy component
+  return () => null;
+};
+
+export default codegenNativeComponent;
