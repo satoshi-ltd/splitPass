6f19969ae33dd72a4bff3e38371b71e4ac254350
soyjavi <hello@soyjavi.com>
2025-01-07 10:21:21 +0700

    Use a wrapper for <Notification>

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export * from './Confirm';
 export * from './Form';
 export * from './InputMask';
 export * from './Menu';
+export * from './Notification';
 export * from './Logo';
 export * from './QR';
 export * from './SecretItem';
